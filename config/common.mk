maldini03/vendor_banana

vendor: Include makefile with art definitions
Change-Id: Ied15be62e315db3061dfb216ea6c7f0fdb43aa22
Signed-off-by: mydongistiny <jaysonedson@gmail.com>

config/common.mk
@@ -3,6 +3,9 @@ BANANA_BUILD := true
 
 BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210605.005/7349499:user/release-keys
 
+# Inherit art options
+include vendor/banana/config/art.mk
+
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.com.google.clientidbase=android-google

APOPHIS9283/vendor_evolution

Signed-off-by: Joey Huab <joey@evolution-x.org>

config/common.mk
@@ -151,6 +151,9 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/evolution/overlay
 DEVICE_PACKAGE_OVERLAYS += vendor/evolution/overlay/common
 
+# Inherit art options
+include vendor/evolution/config/art.mk
+
 # Branding
 include vendor/evolution/config/branding.mk
 